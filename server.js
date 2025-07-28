e847a83a0b8281ff3486d6bf51595de51f37fc4f
Satria Rangga <msatriarangga08@gmail.com>
2025-07-28 10:58:11 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const siswa = require('./api/siswa');
 const pembayaranBulan = require('./api/pembayaran_bulan');
+const pembayaranHarian = require('./api/pembayaran_harian');
 const editNama = require('./api/edit_nama');
 const siswaDelete = require('./api/siswa_delete');
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.get('/api/siswa', siswa);
 app.post('/api/siswa', siswa);
 app.patch('/api/pembayaran_bulan', pembayaranBulan);
+app.patch('/api/pembayaran_harian', pembayaranHarian);
 app.patch('/api/edit_nama', editNama);
 app.delete('/api/siswa_delete', siswaDelete);
 
